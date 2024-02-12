8d1814a44eb7451a48a42bb528715b6a98c8851f
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2024-02-11 22:02:56 -0600

    Basic observation rooms (ideally the surrounding brushes would be clipped and shrunk to 16 units thick, but that's a lot more complicated)

--- src/main/java/io/github/gaming32/mc2p2/generator/MapGenerator.java
+++ src/main/java/io/github/gaming32/mc2p2/generator/MapGenerator.java
@@ -1,7 +1,9 @@
 package io.github.gaming32.mc2p2.generator;
 
 import com.demonwav.mcdev.annotations.Translatable;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.LinkedHashMultimap;
+import com.google.common.collect.Maps;
 import com.google.common.collect.Multimap;
 import io.github.gaming32.mc2p2.util.MC2P2Util;
 import io.github.gaming32.mc2p2.vmf.MaterialInfo;
@@ -9,6 +11,7 @@ import io.github.gaming32.mc2p2.vmf.SimpleBrush;
 import io.github.gaming32.mc2p2.vmf.SourceEntity;
 import io.github.gaming32.mc2p2.vmf.SourceMap;
 import io.github.gaming32.mc2p2.vmf.SourceUtil;
+import it.unimi.dsi.fastutil.ints.IntIntPair;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
@@ -19,12 +22,14 @@ import net.minecraft.world.entity.decoration.ArmorStand;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.DoorBlock;
+import net.minecraft.world.level.block.IronBarsBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.DoorHingeSide;
 import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
 import net.minecraft.world.level.levelgen.structure.BoundingBox;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -34,6 +39,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
 
 public class MapGenerator {
     private static final Map<Block, MaterialSet> BRUSH_BLOCKS = Map.of(
@@ -48,7 +54,7 @@ public class MapGenerator {
             "METAL/BLACK_CEILING_METAL_001B"
         )
     );
-    private static final Map<Block, String> DOOR_BLOCKS = Map.of(
+    private static final Map<Block, String> DOOR_BLOCKS = ImmutableMap.of(
         Blocks.OAK_DOOR, "instances/p2editor/door_frame_white.vmf",
         Blocks.DARK_OAK_DOOR, "instances/p2editor/door_frame_black.vmf"
     );
@@ -57,11 +63,15 @@ public class MapGenerator {
     private final BoundingBox area;
     private final AABB aabb;
     private final IssueConsumer issueConsumer;
+
     private final SourceMap.Builder map;
     private final Set<BlockPos> usedBlocks = new HashSet<>();
     private final Multimap<Block, BlockPos> blockLookup = LinkedHashMultimap.create();
     private final List<SourceEntity.EntityConnection> autoConnections = new ArrayList<>();
     private int entityNameId = 1;
+
+    private final Map<BlockPos, BlockState> blockStateCache;
+    private final Function<BlockPos, BlockState> blockStateLookupFunction;
 
     public MapGenerator(ServerLevel level, BoundingBox area, IssueConsumer issueConsumer) {
         this.level = level;
@@ -69,11 +79,123 @@ public class MapGenerator {
         this.aabb = AABB.of(area);
         this.issueConsumer = issueConsumer;
         this.map = initializeMap();
+
+        this.blockStateCache = Maps.newHashMapWithExpectedSize(area.getXSpan() * area.getYSpan() * area.getZSpan());
+        this.blockStateLookupFunction = level::getBlockState;
     }
 
     public SourceMap generate() {
         scanForBrushes();
         generateDoors();
+        generateObservationRooms();
+        convertEntities();
+        for (final var entry : blockLookup.asMap().entrySet()) {
+            issueConsumer.issue(
+                IssueLevel.INFO,
+                Component.translatable("mc2p2.issue.message.unknown_block", entry.getKey().getName()),
+                entry.getValue()
+            );
+        }
+        if (!autoConnections.isEmpty()) {
+            generateLogicAuto();
+        }
+        return map.build();
+    }
+
+    private void generateObservationRooms() {
+        final Collection<BlockPos> glassBlocks = blockLookup.get(Blocks.GLASS);
+        for (final BlockPos pane : blockLookup.removeAll(Blocks.GLASS_PANE)) {
+            if (usedBlocks.contains(pane)) continue;
+            final Direction facing = getGlassPaneDirection(pane, true);
+            if (facing == null) continue;
+            boolean foundMatch = false;
+            for (int width = 4; width >= 1; width--) {
+                final BoundingBox match = MC2P2Util.matchBlocks(
+                    pane, IntIntPair.of(width, 2), facing.getCounterClockWise(), Direction.UP,
+                    otherPos -> otherPos.equals(pane) || getGlassPaneDirection(otherPos, false) == facing
+                );
+                if (match == null) continue;
+                foundMatch = true;
+                BlockPos.betweenClosedStream(match)
+                    .map(BlockPos::immutable)
+                    .forEach(usedBlocks::add);
+                BlockPos.betweenClosedStream(MC2P2Util.moved(match, facing.getOpposite()))
+                    .map(BlockPos::immutable)
+                    .forEach(e -> {
+                        usedBlocks.add(e);
+                        glassBlocks.remove(e);
+                    });
+                final boolean hasRoom = BlockPos.betweenClosedStream(
+                    MC2P2Util.moved(match, facing.getOpposite(), 4)
+                        .encapsulate(pane.relative(facing.getOpposite(), 2))
+                ).map(this::getBlockState).allMatch(s -> s.isAir() && !s.is(Blocks.VOID_AIR));
+                if (!hasRoom) {
+                    issue(IssueLevel.WARN, "observation_room_no_room", pane);
+                }
+                map.entity(SourceEntity.builder("func_instance")
+                    .origin(SourceUtil.transform(aabb, new Vec3(match.minX(), match.minY() + 1, match.minZ()).relative(facing.getClockWise(), 0.5 * width)))
+                    .angles(new Vec3(0, SourceUtil.transformRotation(facing.toYRot()), 0))
+                    .property("file", "instances/labs/observation_room_" + width * 64 + "x128_1.vmf")
+                    .build()
+                );
+                break;
+            }
+            if (!foundMatch) {
+                issue(IssueLevel.ERROR, "no_observation_room", pane);
+            }
+        }
+    }
+
+    @Nullable
+    private Direction getGlassPaneDirection(BlockPos pos, boolean createIssues) {
+        final Direction direction = getGlassPaneDirectionNoBlockCheck(pos, createIssues);
+        if (direction != null && !getBlockState(pos.relative(direction.getOpposite())).is(Blocks.GLASS)) {
+            if (createIssues) {
+                issue(IssueLevel.ERROR, "glass_pane_not_glass", pos);
+            }
+            return null;
+        }
+        return direction;
+    }
+
+    @Nullable
+    private Direction getGlassPaneDirectionNoBlockCheck(BlockPos pos, boolean createIssues) {
+        final BlockState state = getBlockState(pos);
+        if (!state.is(Blocks.GLASS_PANE)) {
+            return null;
+        }
+        final boolean north = state.getValue(IronBarsBlock.NORTH);
+        final boolean south = state.getValue(IronBarsBlock.SOUTH);
+        final boolean east = state.getValue(IronBarsBlock.EAST);
+        final boolean west = state.getValue(IronBarsBlock.WEST);
+        final int directionCount = z2i(north) + z2i(south) + z2i(east) + z2i(west);
+        if (directionCount == 3) {
+            if (!north) {
+                return Direction.NORTH;
+            }
+            if (!south) {
+                return Direction.SOUTH;
+            }
+            if (!east) {
+                return Direction.EAST;
+            }
+            if (!west) {
+                return Direction.WEST;
+            }
+        }
+        if (createIssues) {
+            if (directionCount < 2) {
+                issue(IssueLevel.ERROR, "glass_pane_small", pos);
+            } else if (directionCount == 2) {
+                issue(IssueLevel.ERROR, "glass_pane_not_glass", pos);
+            } else if (directionCount > 3) {
+                issue(IssueLevel.ERROR, "glass_pane_too_linked", pos);
+            }
+        }
+        return null;
+    }
+
+    private void convertEntities() {
         for (final Entity entity : level.getEntities(null, aabb)) {
             if (entity instanceof ArmorStand armorStand) {
                 map.entity(SourceEntity.builder("info_player_start")
@@ -89,19 +211,6 @@ public class MapGenerator {
                 );
             }
         }
-        if (!blockLookup.isEmpty()) {
-            for (final var entry : blockLookup.asMap().entrySet()) {
-                issueConsumer.issue(
-                    IssueLevel.INFO,
-                    Component.translatable("mc2p2.issue.message.unknown_block", entry.getKey().getName()),
-                    entry.getValue()
-                );
-            }
-        }
-        if (!autoConnections.isEmpty()) {
-            generateLogicAuto();
-        }
-        return map.build();
     }
 
     private void generateLogicAuto() {
@@ -116,18 +225,18 @@ public class MapGenerator {
         for (final var doorBlockType : DOOR_BLOCKS.entrySet()) {
             for (final BlockPos door : blockLookup.removeAll(doorBlockType.getKey())) {
                 if (!usedBlocks.add(door)) continue;
-                final BlockState state = level.getBlockState(door);
+                final BlockState state = getBlockState(door);
                 if (state.getValue(DoorBlock.HALF) != DoubleBlockHalf.LOWER) continue;
                 final Direction facing = state.getValue(DoorBlock.FACING);
                 if (state.getValue(DoorBlock.HINGE) != DoorHingeSide.LEFT) {
-                    final BlockState neighbor = level.getBlockState(door.relative(facing.getCounterClockWise()));
+                    final BlockState neighbor = getBlockState(door.relative(facing.getCounterClockWise()));
                     if (!neighbor.is(doorBlockType.getKey()) || neighbor.getValue(DoorBlock.FACING) != facing) {
                         issue(IssueLevel.ERROR, "unpaired_right_door", door);
                     }
                     continue;
                 }
                 final BlockPos neighborPos = door.relative(facing.getClockWise());
-                final BlockState neighbor = level.getBlockState(neighborPos);
+                final BlockState neighbor = getBlockState(neighborPos);
                 if (!neighbor.is(doorBlockType.getKey()) || neighbor.getValue(DoorBlock.FACING) != facing) {
                     issue(IssueLevel.ERROR, "unpaired_left_door", door);
                     continue;
@@ -173,7 +282,7 @@ public class MapGenerator {
 
     private void scanForBrushes() {
         BlockPos.betweenClosedStream(area).forEach(pos -> {
-            final BlockState state = level.getBlockState(pos);
+            final BlockState state = getBlockState(pos);
             if (state.isAir()) return;
             final MaterialSet materials = BRUSH_BLOCKS.get(state.getBlock());
             if (materials != null) {
@@ -192,7 +301,7 @@ public class MapGenerator {
             final int end = MC2P2Util.getSide(area, dir);
             while (MC2P2Util.getSide(brushBounds, dir) != end) {
                 final BoundingBox newBlocks = MC2P2Util.oneSided(brushBounds, dir).move(dir.getNormal());
-                if (BlockPos.betweenClosedStream(newBlocks).anyMatch(b -> usedBlocks.contains(b) || !level.getBlockState(b).is(type))) {
+                if (BlockPos.betweenClosedStream(newBlocks).anyMatch(b -> usedBlocks.contains(b) || !getBlockState(b).is(type))) {
                     break;
                 }
                 brushBounds.encapsulate(newBlocks);
@@ -206,6 +315,13 @@ public class MapGenerator {
             ),
             materials.map
         ));
+    }
+
+    private BlockState getBlockState(BlockPos pos) {
+        if (!area.isInside(pos)) {
+            return Blocks.VOID_AIR.defaultBlockState();
+        }
+        return blockStateCache.computeIfAbsent(pos, blockStateLookupFunction);
     }
 
     private void issue(IssueLevel level, @Translatable(prefix = "mc2p2.issue.message.") String message, BlockPos... blocks) {
@@ -255,6 +371,10 @@ public class MapGenerator {
                 new AABB(0, ySize64, 0, xSize64, ySize64 + 16, zSize64),
                 Map.of(Direction.DOWN, skyboxMaterial)
             ));
+    }
+
+    private static int z2i(boolean z) {
+        return z ? 1 : 0;
     }
 
     private record MaterialSet(String wall, String floor, String ceiling, Map<Direction, MaterialInfo> map) {
--- src/main/java/io/github/gaming32/mc2p2/util/MC2P2Util.java
+++ src/main/java/io/github/gaming32/mc2p2/util/MC2P2Util.java
@@ -1,13 +1,19 @@
 package io.github.gaming32.mc2p2.util;
 
+import it.unimi.dsi.fastutil.ints.IntIntPair;
+import it.unimi.dsi.fastutil.longs.Long2ObjectMap;
+import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
 import net.minecraft.Util;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.levelgen.structure.BoundingBox;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.EnumMap;
 import java.util.Map;
+import java.util.function.Predicate;
 
 public class MC2P2Util {
     public static final Direction[] DIRECTIONS = Direction.values();
@@ -58,5 +64,68 @@ public class MC2P2Util {
             case WEST -> new BoundingBox(box.minX(), box.minY(), box.minZ(), box.minX(), box.maxY(), box.maxZ());
             case EAST -> new BoundingBox(box.maxX(), box.minY(), box.minZ(), box.maxX(), box.maxY(), box.maxZ());
         };
+    }
+
+    @Nullable
+    public static BoundingBox matchBlocks(
+        BlockPos origin, IntIntPair maxSearch, Direction axis1, Direction axis2, Predicate<BlockPos> predicate
+    ) {
+        final BlockPos.MutableBlockPos mutable = new BlockPos.MutableBlockPos();
+        final BlockPos.MutableBlockPos mutable2 = new BlockPos.MutableBlockPos();
+        final Long2ObjectMap<Boolean> predicateCache = new Long2ObjectOpenHashMap<>();
+        for (int i = 0; i < maxSearch.leftInt(); i++) {
+            for (int j = 0; j < maxSearch.rightInt(); j++) {
+                final BoundingBox match = matchBlocksStep(
+                    mutable.set(origin).move(axis1.getOpposite(), i).move(axis2.getOpposite(), j),
+                    maxSearch, axis1, axis2, predicate, predicateCache, mutable2
+                );
+                if (match != null) {
+                    return match;
+                }
+            }
+        }
+        return null;
+    }
+
+    @Nullable
+    private static BoundingBox matchBlocksStep(
+        BlockPos origin, IntIntPair maxSearch, Direction axis1, Direction axis2,
+        Predicate<BlockPos> predicate, Long2ObjectMap<Boolean> predicateCache,
+        BlockPos.MutableBlockPos mutable
+    ) {
+        for (int i = 0; i < maxSearch.leftInt(); i++) {
+            for (int j = 0; j < maxSearch.rightInt(); j++) {
+                mutable.set(origin).move(axis1, i).move(axis2, j);
+                Boolean matches = predicateCache.get(mutable.asLong());
+                if (matches == null) {
+                    matches = predicate.test(mutable);
+                    predicateCache.put(mutable.asLong(), matches);
+                }
+                if (!matches) {
+                    return null;
+                }
+            }
+        }
+        // These can be left as mutable, because they're leaving the scope they were created in, so they shouldn't be
+        // attempted to be mutated anymore.
+        return BoundingBox.fromCorners(
+            origin,
+            mutable.set(origin)
+                .move(axis1, maxSearch.leftInt() - 1)
+                .move(axis2, maxSearch.rightInt() - 1)
+        );
+    }
+
+    public static BoundingBox copy(BoundingBox box) {
+        return new BoundingBox(box.minX(), box.minY(), box.minZ(), box.maxX(), box.maxY(), box.maxZ());
+    }
+
+    public static BoundingBox moved(BoundingBox box, Direction direction) {
+        return moved(box, direction, 1);
+    }
+
+    @SuppressWarnings("deprecation")
+    public static BoundingBox moved(BoundingBox box, Direction direction, int amount) {
+        return copy(box).move(direction.getNormal().multiply(amount));
     }
 }
